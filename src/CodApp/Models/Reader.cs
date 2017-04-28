8da971e7b064d64728c69aed10c5362a725eb963
Sam Kirsch <samdkirsch@gmail.com>
2017-04-28 09:14:42 -0700

    fill out the reader model and create database migration

--- src/CodApp/Models/Reader.cs
+++ src/CodApp/Models/Reader.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CodApp.Models
 {
+    [Table("Readers")]
     public class Reader
     {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
     }
 }
