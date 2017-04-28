932af1f8fbd4bca33585b66dd95586c8c22bb095
Sam Kirsch <samdkirsch@gmail.com>
2017-04-28 13:18:44 -0700

    rework the home index model to take a dictionary of articles so they can be edited

--- src/CodApp/Controllers/HomeController.cs
+++ src/CodApp/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CodApp.Models;
+using Microsoft.AspNetCore.Identity;
+using CodApp.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +15,18 @@ namespace CodApp.Controllers
     public class HomeController : Controller
     {
         // GET: /<controller>/
+        private CodAppDbContext _db;
+        public HomeController(CodAppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var currentArticle = _db.Articles.FirstOrDefault(m => m.Id == 1);
+            Dictionary<string, Article> homeDictionary = new Dictionary<string, Article> { };
+            homeDictionary["Banner"] = currentArticle;
+            return View(homeDictionary);
         }
     }
 }
