b581a963bbdb7048d3d7abb8ecab90001b5e6de2
Sam Kirsch <samdkirsch@gmail.com>
2017-04-28 08:47:53 -0700

    create the base level controllers and views create the DbContext Model and populate it

---
+++ src/CodApp/Models/CodAppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodApp.Models
+{
+    public class CodAppDbContext : IdentityDbContext<Admin>
+    {
+        public CodAppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Reader> Readers { get; set; }
+    }
+}
--- src/CodApp/Startup.cs
+++ src/CodApp/Startup.cs
@@ -7,20 +7,48 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using CodApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace CodApp
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFramework()
+                .AddDbContext<CodAppDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddIdentity<Admin, IdentityRole>()
+                .AddEntityFrameworkStores<CodAppDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStaticFiles();
+            app.UseIdentity();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
@@ -30,7 +58,7 @@ namespace CodApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Loading page...");
             });
         }
     }
