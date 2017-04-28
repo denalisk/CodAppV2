cd9d0ed73b8737183383763c0f2143e96a00a968
Sam Kirsch <samdkirsch@gmail.com>
2017-04-28 09:03:05 -0700

    create the basic register action and view for admins

--- src/CodApp/Controllers/AdministratorController.cs
+++ src/CodApp/Controllers/AdministratorController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CodApp.Models;
+using Microsoft.AspNetCore.Identity;
+using CodApp.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
